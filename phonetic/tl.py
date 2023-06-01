IepIweidieng/common-tl

commit 3baf54ae36d4b3eaf9ac7d6400b14880b96b9446

Implement a new dialect system, based on `namedtuple`
* phonetic/ctl_util.py: Add help functions and help classes to support the new dialect system
* phonetic/: common_tl.py & tl.py: Use the new help functions to define the accent variants.
* phonetic/*.py: `*_syllable_to_ipa()`: Accept parameters `dialect` and `variant`.
* phonetic/common_tl.py: `ipa_pair_to_tl_pair()`: Accept parameter `dialect` and `variant`.
* ch2rm.py: `ipa_pair_to_tl()`: Forward the arguments to `ipa_pair_to_tl_pair()`.
* ch2rm.py: `phonetic_word_to_ipa()` & `chinese_to_roman()`: Accept parameter `dialects` which forwards the arguments for `*_syllable_to_ipa()`.
* ch2rm.py: `demonstrate()` & the main code: Update the usages for related functions.

phonetic/tl.py
@@ -1,11 +1,26 @@
 import sys
 
+from . import ctl_util
 from .ctl_util import str_get_tone, str_get_gready
 
 
 # Used by tl_syllable_to_ipa
 
 _PHONE_NAME = 'TL'
+
+Dialect = ctl_util.def_dialect(
+      ['chiang', 'choan'])
+#       漳        泉
+dialect = ctl_util.namedtuple_ctor(Dialect)
+
+GENERAL_VARIANT_LIST = ['southern', 'northern']
+VARIANT_LIST = Dialect(
+    chiang=[],
+    choan=[],
+)
+
+Variant = ctl_util.def_variant(GENERAL_VARIANT_LIST, VARIANT_LIST)
+variant = ctl_util.namedtuple_ctor(Variant)
 
 _TONE_PREFIX = ''
 
@@ -65,8 +80,8 @@ _TL_INITIAL_LIST = {
                 'tsh': ['t', _s, 'ʰ'],
                 'ch': ['t', _s],
                 'chh': ['t', _s, 'ʰ'],
-                'dj': [('d', ''), _z],  # For Taiwanese Choân-chiu accent
-                'j': [('d', ''), _z],   # For Taiwanese Chiang-chiu accent
+                'dj': [Dialect('d', ''), _z],  # For Taiwanese Choân-chiu accent
+                'j': [Dialect('d', ''), _z],   # For Taiwanese Chiang-chiu accent
                 's': [_s],                     'h': 'h',
 }
 
@@ -86,8 +101,7 @@ def _O_BRANCH(tl_coda):
     return {
         'ng': 'ɔ', 'm': 'ɔ', 'p': 'ɔ', 'k': 'ɔ',
         'nn': 'ɔ', 'ⁿ': 'ɔ'
-    }.get(tl_coda, ('o',    # For Taiwanese northern accent
-                    'ə' ))  # For Taiwanese southern accent
+    }.get(tl_coda, variant(northern='o', southern='ə'))
 
 def _E_BRANCH(tl_coda):
     return {
@@ -186,11 +200,13 @@ _FINAL_LIST = (
 )'''
 
 
-def tl_syllable_to_ipa(tl_, use_north=False, use_choan=False):
+def tl_syllable_to_ipa(tl_, dialect='chiang', variant='southern'):
     """
     Convert a TL syllable to IPA. \n
     Side effect: IO (w)
     """
+    dialect = dialect and dialect.replace("'", '_').lower()
+    variant = variant and variant.replace("'", '_').lower()
 
     (tone, tl_no_tone) = str_get_tone(tl_, _TL_TONE_LIST, _NULL_TONE_BRANCH)
 
@@ -225,8 +241,6 @@ def tl_syllable_to_ipa(tl_, use_north=False, use_choan=False):
         if is_initial:
             if callable(result):
                 result = result(tl_medial or tl_nucleus0)
-            if isinstance(result, tuple):
-                result = result[use_choan and 0 or 1]
         else:
             if callable(result):
                 result = result(tl_coda)
@@ -234,8 +248,10 @@ def tl_syllable_to_ipa(tl_, use_north=False, use_choan=False):
                 result = result(tl_medial)
             if callable(result):
                 result = result(tl_initial)
-            if isinstance(result, tuple):
-                result = result[use_north and 0 or 1]
+        if isinstance(result, Dialect):
+            result = getattr(result, dialect)
+        if isinstance(result, Variant):
+            result = getattr(result, variant)
         if isinstance(result, list):
             new_result = [get_patched(ipa_part, is_initial) for ipa_part in result]
             result = ''.join(new_result)

ch2rm.py
@@ -1,8 +1,14 @@
 import ctl_dict
 import ctl_segment
+from phonetic import ctl_util
 from phonetic.common_tl import ipa_pair_to_tl_pair
 from phonetic.zhuyin import zhuyin_syllable_to_ipa
 from phonetic.tl import tl_syllable_to_ipa
+
+lang_opt = ctl_util.lang_opt
+Lang = ctl_util.def_lang(
+      ['hokkien', 'mandarin', 'common_tl'])
+lang = ctl_util.namedtuple_ctor(Lang, default=lang_opt())
 
 
 def chinese_word_to_phonetic(word, dict_):
@@ -13,7 +19,7 @@ def chinese_word_to_phonetic(word, dict_):
     return dict_.chinese_phonetic.get(word, [])
 
 
-def phonetic_word_to_ipa(phonetic_word, use_north=False, use_choan=False):
+def phonetic_word_to_ipa(phonetic_word, dialects=lang()):
     """
     將拼音轉成國際音標 \n
     Convert a phonetic word into IPA. \n
@@ -25,22 +31,24 @@ def phonetic_word_to_ipa(phonetic_word, use_north=False, use_choan=False):
 
     for syllable in phonetic_word:
         if isinstance(syllable, ctl_dict.TL):
-            ipa_word.append(tl_syllable_to_ipa(syllable, use_north, use_choan))
+            ipa_word.append(
+                tl_syllable_to_ipa(syllable, **getattr(dialects, 'hokkien')._asdict()))
         elif isinstance(syllable, ctl_dict.Zhuyin):
-            ipa_word.append(zhuyin_syllable_to_ipa(syllable))
+            ipa_word.append(
+                zhuyin_syllable_to_ipa(syllable, **getattr(dialects, 'mandarin')._asdict()))
 
     return ipa_word
 
 
-def ipa_pair_to_tl(ipa_pair, use_north=False):
+def ipa_pair_to_tl(ipa_pair, *args, **kwargs):
     """
     將國際音標轉成廣義臺羅拼音 \n
     Convert an IPA initial-final pair into common TL initial-final pair. \n
     """
-    return [ipa_pair_to_tl_pair(syllable, use_north) for syllable in ipa_pair]
+    return [ipa_pair_to_tl_pair(syllable, *args, **kwargs) for syllable in ipa_pair]
 
 
-def chinese_to_roman(sentence, dict_, use_north=False, use_choan=False):
+def chinese_to_roman(sentence, dict_, dialects=lang()):
     """
     Convert a Chinese sentence to Roman. \n
     Side effect:
@@ -54,8 +62,10 @@ def chinese_to_roman(sentence, dict_, use_north=False, use_choan=False):
         # Currently only use the first phonetic of candidate phonetics
         candidate_phonetic_word = chinese_word_to_phonetic(word, dict_)[0:1]
         for phonetic_word in candidate_phonetic_word:
-            ipa_pair_word = phonetic_word_to_ipa(phonetic_word, use_north, use_choan)
-            tl_pair_word = ipa_pair_to_tl(ipa_pair_word, use_north)
+            ipa_pair_word = phonetic_word_to_ipa(phonetic_word, dialects)
+            tl_pair_word = ipa_pair_to_tl(ipa_pair_word,
+                **(getattr(dialects, 'common_tl')._asdict()
+                    or getattr(dialects, 'hokkien')._asdict()))
 
             tl_pair_list.append(tl_pair_word)
 
@@ -91,8 +101,8 @@ def demonstrate():
     print('Loading time: ', time_loading_end - time_loading_start, ' s',
           sep='', end='\n\n')
 
-    use_north = False
-    use_choan = False
+    dialect = 'chiang'
+    variant = 'southern'
 
     sentence = (
         ' 測 試  這   兒 巴  陵郡  日zZㄈ=心  謗腹非 拔 了一 個 尖  兒八   面'
@@ -112,8 +122,9 @@ def demonstrate():
             # Currently only use the first phonetic of candidate phonetics
             candidate_phonetic_word = chinese_word_to_phonetic(word, dict_)[0:1]
             for phonetic_word in candidate_phonetic_word:
-                ipa_pair_word = phonetic_word_to_ipa(phonetic_word, use_north, use_choan)
-                tl_pair_word = ipa_pair_to_tl(ipa_pair_word, use_north)
+                ipa_pair_word = phonetic_word_to_ipa(
+                    phonetic_word, lang(hokkien=lang_opt(dialect, variant)))
+                tl_pair_word = ipa_pair_to_tl(ipa_pair_word, dialect, variant)
                 output = (
                     f'{output}'
                     f'{" ".join(map(str, phonetic_word))}\t'
@@ -151,4 +162,5 @@ if __name__ == '__main__':
     dict_ = dict_src.create_dict()
 
     print(chinese_to_roman(sentence, dict_))
-    print(chinese_to_roman(sentence, dict_, use_north=True))
+    print(chinese_to_roman(sentence, dict_,
+        dialects=lang(hokkien=lang_opt(variant='northern'))))

phonetic/common_tl.py
@@ -1,4 +1,15 @@
+from . import ctl_util
+
 # Used by ipa_pair_to_tl_pair
+
+Dialect = ctl_util.def_dialect([])
+dialect = ctl_util.namedtuple_ctor(Dialect)
+
+GENERAL_VARIANT_LIST = ['southern', 'northern']
+VARIANT_LIST = Dialect()
+
+Variant = ctl_util.def_variant(GENERAL_VARIANT_LIST, VARIANT_LIST)
+variant = ctl_util.namedtuple_ctor(Variant)
 
 _COMMON_TL_INITIAL_LIST = {
     # *: Not in original TL
@@ -47,17 +58,17 @@ _COMMON_TL_FINAL_LIST = {
 
     'ɘ': 'er',  # Used in Taiwanese Choân-chiu accent
 
-    'ɤ': (
-        'or',  # For Taiwanese northern accent
-        'o'),  # For Taiwanese southern accent
-    'ə': (
-        'or',
-        'o'),
+    'ɤ': variant(
+        northern='or',  # For Taiwanese northern accent
+        southern='o'),  # For Taiwanese southern accent
+    'ə': variant(
+        northern='or',
+        southern='o'),
     #   They are allophones.  Use only one symbol for them.
 
-    'ʊ': (
-        'o',  # More accurate transcription for Taiwanese northern accent
-        'oo'),
+    'ʊ': variant(
+        northern='o',  # More accurate transcription for Taiwanese northern accent
+        southern='oo'),
 
     'ɔ': 'oo',
 
@@ -68,9 +79,9 @@ _COMMON_TL_FINAL_LIST = {
                 #   In these conditions, replace the 'ee' with 'a' later.
 
     # Still IPA vowels. Erization-related part.
-    'ɚ': (
-        'orrr',    # * Rhotic vowel; see u'\u02DE' ' ˞ '.
-        'orr'),
+    'ɚ': variant(
+        northern='orrr',    # * Rhotic vowel; see u'\u02DE' ' ˞ '.
+        southern='orr'),
 
     'ɐ': 'a',  # Produced by rhotic bopomofo finals "ㄧㄢㄦ" and "ㄩㄢㄦ"
 
@@ -134,7 +145,7 @@ def _replace_symbol(src, prev_pos_list, prev_replace_pair,
     return new_src
 
 
-def ipa_pair_to_tl_pair(ipa_pair, use_north=False):
+def ipa_pair_to_tl_pair(ipa_pair, dialect=None, variant='southern'):
     """Convert an IPA syllable to common TL."""
     (ipa_initial, ipa_final) = ipa_pair
     (tl_initial, tl_final) = ('', '')
@@ -151,8 +162,8 @@ def ipa_pair_to_tl_pair(ipa_pair, use_north=False):
     for ipa_phone in ipa_final:
         tl_phone = _COMMON_TL_FINAL_LIST.get(ipa_phone, ipa_phone)
 
-        if isinstance(tl_phone, tuple):
-            tl_phone = tl_phone[not use_north and 1 or 0]
+        if isinstance(tl_phone, Variant):
+            tl_phone = getattr(tl_phone, variant)
 
         # Replace 'ieen' with 'ian',     'yeen' with 'yan'
         #         'ieet' with 'iat', and 'yeet' with 'yat'

phonetic/ctl_util.py
@@ -1,3 +1,22 @@
+from collections import namedtuple
+
+def namedtuple_ctor(Tuple, default=None):
+    return (lambda *args, **kwargs:
+        Tuple(*([*args] + [default]*(len(Tuple._fields)-len(args))))._replace(**kwargs))
+
+def def_lang(lang_list):
+    return namedtuple('Lang', lang_list)
+def def_dialect(dialect_list):
+    return namedtuple('Dialect', dialect_list)
+def def_variant(general_variant_list, variant_list):
+    return namedtuple('Variant', sum(variant_list, general_variant_list))
+
+class Lang_opt(namedtuple('Lang_opt', ['dialect', 'variant'])):
+    def _asdict(self):
+        res = super()._asdict()
+        return {k: res[k] for k in res if res[k] is not None}
+lang_opt = namedtuple_ctor(Lang_opt)
+
 def find_first_non_roman(text):
     """
     Usage & result:

phonetic/zhuyin.py
@@ -104,7 +104,7 @@ def _final_branch(final, bopomofo_rhyme):
 
 
 
-def zhuyin_syllable_to_ipa(zhuyin):
+def zhuyin_syllable_to_ipa(zhuyin, dialect=None, variant=None):
     """
     Convert a zhuyin syllable to IPA. \n
     Side effect: IO (w)

commit 1f316a8d05527117d16bef3db155bbb68c6ccd90

fix(tl.py): fix CTL "dj" vs. "j" wrongly swapped, introduced in 3baf54ae36d4b3eaf9ac7d6400b14880b96b9446
CTL "dj" is for transcribing the affricate pronunciation of TL "j",
which occurs in Choân-chiu accents.

CTL "j" is both for transcribing the fricative pronunciation
and for simply transcribing the phoneme
without distinguishing between the fricative and affricate pronunciation.

In 3baf54ae36d4b3eaf9ac7d6400b14880b96b9446,
where the `namedtuple`-based dialect system was introduced,
the transcription definition for Chiang-chiu and Choân-chiu was wrongly swapped.

To address this issue, the transcription definition is swapped back.

phonetic/tl.py
@@ -65,7 +65,7 @@ def _j(self_type: Part, ipa: SrcParts) -> PhoneSpec:
     if self_type == phonetic.INITIAL:
         if after_initial(ipa) == 'i' and ipa.coda[0] and ipa.coda[0].startswith('nn'):
             return 'ȵ' # = TL 'gn'
-        return [Dialect('d', ''), _z]
+        return [Dialect('', 'd'), _z]
     return _j
 
 